8e4acc33cd22689ecc4461968dad4f8f22f48554
Chernyshov <xpom55@mail.ru>
2018-07-25 20:46:58 +0300

    add build command

--- src/index.js
+++ src/index.js
@@ -16,7 +16,26 @@ pgp.pg.defaults.ssl = true;
 
 const db = pgp('postgres://ouhvltpduxmogf:14bd371be3883441bcb4d6d1a1aa1d990f8677665a7bc203a765069129b8e6c5@ec2-54-247-123-231.eu-west-1.compute.amazonaws.com:5432/d9fej1ob3jfs9u');
 
+// db.one('CREATE TABLE demo_1(a int);')
+//   .then((data) => {
+//     console.log(`Data: ${data}`)
+//   })
+//   .catch((err) => console.log(`error: ${err}`));
+// db.one('INSERT INTO demo_1(a) VALUES($1) RETURNING a', [321])
+//   .then(data => {
+//     console.log(data.id); // print new user id;
+//   })
+//   .catch(error => {
+//     console.log('ERROR:', error); // print error;
+//   });
 
+db.any("SELECT * FROM demo_1;")
+  .then(function (data) {
+    console.log("demo_1:", data);
+  })
+  .catch(function (error) {
+    console.log("ERROR:", error);
+  });
 
 app.get('/', function(req, res) {
   res.send(`<div>
