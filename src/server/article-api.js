60b80e04fd8f748dfe3e219647caa6dbccd460c8
Chernyshov <xpom55@mail.ru>
2018-08-07 23:29:40 +0300

    add article-api.js and test it

--- src/server/article-api.js
+++ src/server/article-api.js
@@ -1,56 +1,69 @@
 import { Router } from 'express';
 import db from './db/postgreSQL';
+import * as handlers from './db/then-catch-handlers';
 
 const router = Router();
-const table = 'article';
+const table = `article`;
 
-router.get('/article', function(req, res) {
-  // http://localhost:8080/api/article
+// http://localhost:8080/api/article
+router.get(`/article`, function(req, res) {
   console.log(`GET: ${table} path`);
 
-  db.any(`SELECT * FROM article`)
-    .then((data) => {
-      console.log(table, data);
-      res.status(200).send(`<h1>${table}, ${data}!</h1>`);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(505);
-    });
+  db.any(`SELECT * FROM article;`)
+    .then(handlers.selectThen(res))
+    .catch(handlers.selectCatch(res));
 });
 
+// curl -d '{"work_id": 1, "article": "LIDT-4", "time": 120, "amount": 400, "boxes": 7, "in_box": 60, "plus_box": 40}' -H "Content-Type: application/json" -X POST http://localhost:8080/api/article
+// Создает полную новую запись
 router.post('/article', function(req, res) {
   console.log(`POST: ${table} path`, req.body);
+  const { work_id, article, time, amount, boxes, in_box, plus_box } = req.body;
+  db.one(
+    `INSERT INTO article(
+    article_id, work_id, article, time, amount, boxes, in_box, plus_box
+  ) VALUES(DEFAULT, $1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
+    [work_id, article, time, amount, boxes, in_box, plus_box]
+  )
+    .then(handlers.postThen)
+    .catch(handlers.postCatch);
+});
+
+// curl -d '{"work_id": 1, "article": "LIDT-4-2", "time": 120, "amount": 400, "boxes": 7, "in_box": 60, "plus_box": 40}' -H "Content-Type: application/json" -X PUT http://localhost:8080/api/article/1
+// Обновляет запись по ID
+router.put(`/article/:id`, (req, res) => {
+  console.log(`PUT: ${table} path`, req.body);
+
+  const { work_id, article, time, amount, boxes, in_box, plus_box } = req.body;
 
   db.one(
-    'INSERT INTO article(article_id, name) VALUES(DEFAULT, $2) RETURNING employee_id',
-    [req.body.name]
+    `UPDATE article SET
+      work_id = '${work_id}',
+      article = '${article}',
+      time = '${time}',
+      amount = '${amount}',
+      boxes = '${boxes}',
+      in_box = '${in_box}',
+      plus_box = '${plus_box}'
+    WHERE article_id = ${req.params.id}
+    RETURNING *;`
   )
-    .then((name) => console.log('return name ', name))
-    .catch((err) => console.log('error', err));
+    .then(handlers.putThen)
+    .catch(handlers.putCatch);
+});
+
+// curl -H "Content-Type: application/json" -X DELETE http://localhost:8080/api/article/2
+// Удаляет запись по id
+router.delete(`/article/:id`, ({ params: { id } }, req) => {
+  console.log(`DELETE: ${table} by ID ${id}`);
+
+  db.one(`DELETE FROM article WHERE article_id = ${id} RETURNING *;`)
+    .then(handlers.deleteThen)
+    .catch(handlers.deleteCatch);
 });
 
 export default router;
 
-// app.listen(port, function() {
-//   console.log(`app listening on port ${port}!`);
-// });
-
-// db.one('INSERT INTO employee(employee_id, name) VALUES($1, $2) RETURNING name', [2, 'Вася'])
-//   .then((name) => console.log('return name ', name))
-//   .catch((err) => console.log('error', err));
-
-// work, employee, article, rate CREATE TABLES
-// db.one(`CREATE TABLE work(
-//   work_id bigserial primary key,
-
-//   date timestamp NOT NULL,
-
-//   employee_id BIGINT references employee(employee_id) NOT NULL,
-//   name_day_id BIGINT NOT NULL,
-
-//   full_time real NOT NULL
-// );`);
 // db.one(`CREATE TABLE article(
 //   article_id bigserial primary key,
 //   work_id BIGINT references work(work_id) NOT NULL,
@@ -62,19 +75,4 @@ export default router;
 //   in_box BIGINT NOT NULL,
 
 //   plus_box BIGINT NOT NULL
-// );`);
-// db.one(`CREATE TABLE employee(
-//   employee_id bigserial primary key,
-
-//   name text NOT NULL
-// );`);
-// db.one(`CREATE TABLE rate(
-//   rate_id bigserial primary key,
-//   employee_id BIGINT references employee(employee_id) NOT NULL,
-
-//   start_date timestamp NOT NULL,
-//   end_date timestamp NOT NULL,
-
-//   rate_week_day real NOT NULL,
-//   rate_week_end real NOT NULL
 // );`);
